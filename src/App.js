5a602bc89a9075f8cea740510b80bec318108c65
babyazalea <usun12@hotmail.com>
2019-05-28 22:45:07 +0900

    구조에대한 아이디어가 없음

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import logo from './images/keul_logo.png';
 
 function App() {
   return (
     <div className="App">
+       <div className="wrap">
+         <header>
+           <h1><img src={logo} alt="keul logo" className="keulLogo"></img></h1>
+         </header>
+       </div>
     </div>
   );
 }
